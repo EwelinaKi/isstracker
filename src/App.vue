e37b5d8ab6351144a1d9b7530e4c869e0d19e3c4
Ewelina Kijanowska <kijanowska@gmail.com>
2019-05-02 09:30:34 +0200

    google-map boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div id="nav"></div>
+    <GmapMap
+      :center="center"
+      :zoom="2"
+      map-type-id="terrain"
+      style="min-width: 900px; height: 600px; margin: auto"
+    >
+    </GmapMap>
   </div>
 </template>
 
+
+<script>
+
+export default {
+  data() {
+    return {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+  },
+};
+
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
+import * as VueGoogleMaps from 'vue2-google-maps';
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GM_API_KEY,
+    libraries: 'places',
+  },
+});
+
 new Vue({
-  router,
   store,
   render: h => h(App),
 }).$mount('#app');
