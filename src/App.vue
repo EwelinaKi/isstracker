c784a69b146321a26e82636fac089d20176d2505
Ewelina Kijanowska <kijanowska@gmail.com>
2019-05-02 19:16:11 +0200

    css style

--- src/App.vue
+++ src/App.vue
@@ -56,4 +56,26 @@ export default {
 </script>
 
 <style>
+  :root {
+    color: #2c3e50;
+    --color-main: #42b983;
+    --color-main-light: #42b98350;
+    --color-light: #fff;
+    --color-dark: #35495E;
+    --color-dark-light: #35495E30;
+    --radius: 10px;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
--- src/components/ButtonsBar.vue
+++ src/components/ButtonsBar.vue
@@ -29,5 +29,99 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+  body {
+    margin: 0;
+  }
+
+  #nav {
+    height: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+  }
+
+  #nav, .buttons {
+    flex-wrap: wrap;
+  }
+
+  .buttons {
+    max-width: 640px;
+    margin: auto;
+  }
+
+  .buttons, .history {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .buttons>div {
+    width: 300px;
+    flex-wrap: nowrap;
+    margin: 10px auto;
+  }
+
+  .input {
+    width: 120px;
+  }
+
+  input {
+    width: 50px ;
+    height: 33px;
+    text-align: center;
+    border: none;
+  }
+
+  button, input {
+    background-color: var(--color-light);
+    border-radius: var(--radius);
+    font-size: medium;
+  }
+
+  button {
+    color: var(--color-main) ;
+    border: solid 2px var(--color-main);
+    width: 120px;
+    height: 40px;
+    margin: 10px;
+  }
+
+  button:hover {
+    color: var(--color-light) !important;
+    background-color:  var(--color-main) ;
+  }
+
+  button:active {
+    background-color: var(--color-dark);
+    border-color: var(--color-dark);
+  }
+
+  button:focus, input:focus {
+    outline:none;
+  }
+
+  h1, .buttons {
+    align-self: center;
+  }
+
+  h1 {
+    max-width: 300px;
+    height: 60px;
+    text-align: center;
+    margin: auto;
+    line-height: 60px;
+  }
+
+  h1 span {
+    color: var(--color-main);
+    font-weight: bolder;
+  }
+
+  .history {
+    border: 1px solid var(--color-dark-light);
+    border-radius: var(--radius);
+  }
 </style>
